38d7ec3cfb9a681fd26320b0e9c2a300ba753496
Özgür Akkurt <oezgurmakkurt@gmail.com>
2019-05-22 10:01:11 +0300

    srot

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
-#[allow(clippy::many_single_char_names)]
+#![allow(clippy::many_single_char_names)]
+
+use core::arch::x86_64::*;
+
+macro_rules! unroll4 {
+    ($e:expr) => {{
+        $e;
+        $e;
+        $e;
+        $e;
+    }};
+}
+const STEP: usize = 8 * 4;
 
 pub fn srotg(a: f32, b: f32) -> (f32, f32, f32, f32) {
     if a == 0.0 && b == 0.0 {
@@ -20,4 +32,53 @@ pub fn srotg(a: f32, b: f32) -> (f32, f32, f32, f32) {
         1.0
     };
     (r, z, c, s)
+}
+
+pub unsafe fn srot(
+    n: usize,
+    mut x: *mut f32,
+    incx: isize,
+    mut y: *mut f32,
+    incy: isize,
+    c: f32,
+    s: f32,
+) {
+    if incx == 1 && incy == 1 {
+        let cv = _mm256_broadcast_ss(&c);
+        let sv = _mm256_broadcast_ss(&s);
+
+        for _ in 0..n / STEP {
+            unroll4!({
+                let xv = _mm256_loadu_ps(x);
+                let yv = _mm256_loadu_ps(y);
+
+                _mm256_storeu_ps(x, _mm256_fmadd_ps(cv, xv, _mm256_mul_ps(sv, yv)));
+                _mm256_storeu_ps(y, _mm256_fmsub_ps(cv, yv, _mm256_mul_ps(sv, xv)));
+
+                x = x.offset(8);
+                y = y.offset(8);
+            });
+        }
+        for _ in 0..n % STEP {
+            let xi = *x;
+            let yi = *y;
+
+            *x = c * xi + s * yi;
+            *y = c * yi - s * xi;
+
+            x = x.offset(1);
+            y = y.offset(1);
+        }
+    } else {
+        for _ in 0..n {
+            let xi = *x;
+            let yi = *y;
+
+            *x = c * xi + s * yi;
+            *y = c * yi - s * xi;
+
+            x = x.offset(incx);
+            y = y.offset(incy);
+        }
+    }
 }
