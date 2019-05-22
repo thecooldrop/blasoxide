ec85f4e518006b3d8053f60798f5f6b36c8c27e7
Özgür Akkurt <oezgurmakkurt@gmail.com>
2019-05-22 09:48:00 +0300

    srotg

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,23 @@
-macro_rules! unroll4 {
-    ($e:expr) => {{
-        $e;
-        $e;
-        $e;
-        $e
-    }};
-}
+#[allow(clippy::many_single_char_names)]
 
-pub unsafe fn sgemm(
-    _transa: bool,
-    _transb: bool,
-    m: usize,
-    n: usize,
-    k: usize,
-    _alpha: f32,
-    a: *const f32,
-    lda: usize,
-    b: *const f32,
-    ldb: usize,
-    _beta: f32,
-    c: *mut f32,
-    ldc: usize,
-) {
-    const BLOCKSIZE: usize = 32;
-    const UNROLL: usize = 4;
-    const VEC_LEN: usize = 8;
-
-    for j in (0..n).step_by(BLOCKSIZE) {
-        for i in (0..m).step_by(BLOCKSIZE) {
-            for p in (0..k).step_by(BLOCKSIZE) {
-                do_block(i, j, p, a, lda, b, ldb, c, ldc);
-            }
-        }
+pub fn srotg(a: f32, b: f32) -> (f32, f32, f32, f32) {
+    if a == 0.0 && b == 0.0 {
+        return (0.0, 0.0, 1.0, 0.0);
     }
-
-    unsafe fn do_block(
-        si: usize,
-        sj: usize,
-        sp: usize,
-        a: *const f32,
-        lda: usize,
-        b: *const f32,
-        ldb: usize,
-        c: *mut f32,
-        ldc: usize,
-    ) {
-        for i in (0..si + BLOCKSIZE).step_by(VEC_LEN * UNROLL) {
-            for j in 0..sj + BLOCKSIZE {
-                for p in 0..sp + BLOCKSIZE {}
-            }
-        }
-    }
+    let h = a.hypot(b);
+    let r = if a.abs() > b.abs() {
+        h * a.signum()
+    } else {
+        h * b.signum()
+    };
+    let c = a / r;
+    let s = b / r;
+    let z = if a.abs() > b.abs() {
+        s
+    } else if c != 0.0 {
+        1.0 / c
+    } else {
+        1.0
+    };
+    (r, z, c, s)
 }
