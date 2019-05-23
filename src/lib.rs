14cac3684ec6c99ce090ae8c1e0fe89c1d645a5d
Özgür Akkurt <oezgurmakkurt@gmail.com>
2019-05-23 05:49:05 +0300

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -139,3 +139,50 @@ pub unsafe fn sscal(n: usize, a: f32, mut x: *mut f32, incx: isize) {
         }
     }
 }
+
+pub unsafe fn scopy(n: usize, mut x: *const f32, incx: isize, mut y: *mut f32, incy: isize) {
+    if incx == 1 && incy == 1 {
+        for _ in 0..n/STEP {
+            unroll4!({
+                _mm256_storeu_ps(y, _mm256_loadu_ps(x));
+                x = x.offset(8);
+                y = y.offset(8);
+            });
+        }
+        for _ in 0..n%STEP {
+            *y = *x;
+            x = x.offset(1);
+            y = y.offset(1);
+        }
+    } else {
+        for _ in 0..n {
+            *y = *x;
+            x = x.offset(incx);
+            y = y.offset(incy);
+        }
+    }
+}
+
+pub unsafe fn saxpy(n: usize, a: f32, mut x: *const f32, incx: isize, mut y: *mut f32, incy: isize) {
+    if incx == 1 && incy == 1 {
+        let av = _mm256_broadcast_ss(&a);
+        for _ in 0..n/STEP {
+            unroll4!({
+                _mm256_storeu_ps(y, _mm256_fmadd_ps(av, _mm256_loadu_ps(x), _mm256_loadu_ps(y)));
+                x = x.offset(8);
+                y = y.offset(8);
+            });
+        }
+        for _ in 0..n%STEP {
+            *y += a * *x;
+            x = x.offset(1);
+            y = y.offset(1);
+        }
+    } else {
+        for _ in 0..n {
+            *y += a * *x;
+            x = x.offset(incx);
+            y = y.offset(incy);
+        }
+    }
+}
