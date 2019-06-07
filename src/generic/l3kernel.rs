cbd36cd84439679e0dc2ac665144f07b0669c047
Özgür Akkurt <oezgurmakkurt@gmail.com>
2019-06-07 14:37:25 +0300

    finish l3kernel of generic

--- src/generic/l3kernel.rs
+++ src/generic/l3kernel.rs
@@ -13,6 +13,41 @@ pub unsafe fn sgemm_16x4_packed(
             return;
         }
     }
+
+    let cptr0 = c;
+    let cptr1 = c.add(ldc);
+    let cptr2 = c.add(ldc * 2);
+    let cptr3 = c.add(ldc * 3);
+
+    for i in 0..16 {
+        let mut creg0 = *cptr0.add(i) * beta;
+        let mut creg1 = *cptr1.add(i) * beta;
+        let mut creg2 = *cptr2.add(i) * beta;
+        let mut creg3 = *cptr3.add(i) * beta;
+        
+        let mut bptr = b;
+
+        for p in 0..k {
+            let areg = *a.add(i + p * 16);
+
+            let breg0 = *bptr;
+            let breg1 = *bptr.add(1);
+            let breg2 = *bptr.add(2);
+            let breg3 = *bptr.add(3);
+
+            creg0 += breg0 * areg;
+            creg1 += breg1 * areg;
+            creg2 += breg2 * areg;
+            creg3 += breg3 * areg;
+
+            bptr = bptr.add(4);
+        }
+
+        *cptr0.add(i) = creg0;
+        *cptr1.add(i) = creg1;
+        *cptr2.add(i) = creg2;
+        *cptr3.add(i) = creg3;
+    }
 }
 
 pub unsafe fn dgemm_8x4_packed(
@@ -29,5 +64,40 @@ pub unsafe fn dgemm_8x4_packed(
             crate::fma::dgemm_8x4_packed(k, a, b, beta, c, ldc);
             return;
         }
+    }
+
+    let cptr0 = c;
+    let cptr1 = c.add(ldc);
+    let cptr2 = c.add(ldc * 2);
+    let cptr3 = c.add(ldc * 3);
+
+    for i in 0..8 {
+        let mut creg0 = *cptr0.add(i) * beta;
+        let mut creg1 = *cptr1.add(i) * beta;
+        let mut creg2 = *cptr2.add(i) * beta;
+        let mut creg3 = *cptr3.add(i) * beta;
+        
+        let mut bptr = b;
+
+        for p in 0..k {
+            let areg = *a.add(i + p * 8);
+
+            let breg0 = *bptr;
+            let breg1 = *bptr.add(1);
+            let breg2 = *bptr.add(2);
+            let breg3 = *bptr.add(3);
+
+            creg0 += breg0 * areg;
+            creg1 += breg1 * areg;
+            creg2 += breg2 * areg;
+            creg3 += breg3 * areg;
+
+            bptr = bptr.add(4);
+        }
+
+        *cptr0.add(i) = creg0;
+        *cptr1.add(i) = creg1;
+        *cptr2.add(i) = creg2;
+        *cptr3.add(i) = creg3;
     }
 }
